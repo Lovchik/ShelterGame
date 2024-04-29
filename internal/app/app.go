df943b922311f67f23157ac107aae70e45a1f391
Vitaly Louchy <vitaly.louchy@indev.by>
2024-04-29 08:50:15 +0300

    added handlers to checkPlayers,clearList, add new player to game

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,8 @@ import (
 	"ShelterGame/internal/dtos"
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+	"gorm.io/gorm/utils"
 	"log/slog"
 	"os"
 )
@@ -16,6 +18,7 @@ func main() {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
+	var gameMembers []string
 
 	updates, _ := bot.UpdatesViaLongPolling(nil)
 	bh, _ := th.NewBotHandler(bot, updates)
@@ -23,36 +26,87 @@ func main() {
 	defer bh.Stop()
 	defer bot.StopLongPolling()
 
+	keyboard := tu.Keyboard(getUsers()).WithSelective().WithResizeKeyboard().WithInputFieldPlaceholder("Select something")
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		user := dtos.User{
-			Username: update.Message.From.Username,
-			ChatId:   update.Message.Chat.ID,
+		var count *int64
+		sqlite.GetDB().Model(&dtos.User{}).Where("chat_id=?", update.Message.Chat.ID).Count(count)
+		if count == nil {
+			user := dtos.User{
+				Username: update.Message.From.Username,
+				ChatId:   update.Message.Chat.ID,
+			}
+			sqlite.GetDB().Model(&dtos.User{}).Debug().Create(&user)
+		} else {
+			_, _ = bot.SendMessage(tu.Message(update.Message.Chat.ChatID(), "You are already signed"))
+
+			slog.Info("User with chat_id: ", update.Message.Chat.ID, "exists")
 		}
-		sqlite.GetDB().Model(&dtos.User{}).Debug().Create(&user)
 	}, th.CommandEqual("start"))
 
-	/*for update := range updates {
-		if update.Message != nil {
-			chatID := tu.ID(update.Message.Chat.ID)
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		_, _ = bot.SendMessage(tu.Message(update.Message.Chat.ChatID(), "You are already signed").WithReplyMarkup(keyboard))
 
-			if update.Message.Text == "Чебурек" {
-				_, err = bot.SendMessage(tu.Message(chatID, "Ну дарова"))
-				if err != nil {
-					return
-				}
-				user := dtos.User{
-					Username: update.Message.From.Username,
-					ChatId:   chatID.ID,
-				}
-				sqlite.GetDB().Model(&dtos.User{}).Create(&user)
+	}, th.CommandEqual("w"))
 
-			} else {
-				_, _ = bot.CopyMessage(tu.CopyMessage(chatID, chatID, update.Message.MessageID))
-
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		if gameMembers != nil {
+			message := ""
+			for _, member := range gameMembers {
+				message += member + "\n"
 			}
+			_, _ = bot.SendMessage(tu.Message(update.Message.Chat.ChatID(), message))
+		} else {
+			_, _ = bot.SendMessage(tu.Message(update.Message.Chat.ChatID(), "No any players"))
 
 		}
 
-	}*/
+	}, th.CommandEqual("checkPlayers"))
+
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		slog.Info(update.Message.Chat.Username)
+		if update.Message.Chat.Username == "Lovchik1Kg" {
+			gameMembers = nil
+		}
+	}, th.CommandEqual("clearList"))
+
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		if update.Message.Text != "" && update.Message.Text[:1] == "-" && isUserExists(update.Message.Text[1:]) {
+			if utils.Contains(gameMembers, update.Message.Text[1:]) {
+				_, _ = bot.SendMessage(tu.Message(update.Message.Chat.ChatID(), update.Message.Text[1:]+" already added"))
+			} else {
+				gameMembers = append(gameMembers, update.Message.Text[1:])
+				_, _ = bot.SendMessage(tu.Message(update.Message.Chat.ChatID(), update.Message.Text[1:]+" successfully added"))
+			}
+		}
+	}, th.Any())
+
 	bh.Start()
+}
+
+func getUsers() []telego.KeyboardButton {
+	var users []dtos.User
+	sqlite.GetDB().Model(&dtos.User{}).Find(&users)
+	var buttons []telego.KeyboardButton
+
+	for _, user := range users {
+		buttons = append(buttons, telego.KeyboardButton{
+			Text:            "-" + user.Username,
+			RequestUsers:    nil,
+			RequestChat:     nil,
+			RequestContact:  false,
+			RequestLocation: false,
+			RequestPoll:     nil,
+			WebApp:          nil,
+		})
+	}
+	return buttons
+}
+
+func isUserExists(username string) bool {
+	var count int64
+	sqlite.GetDB().Model(&dtos.User{}).Where("username=?", username).Count(&count)
+	if count == 0 {
+		return false
+	}
+	return true
 }
